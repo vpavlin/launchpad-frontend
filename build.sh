b5014c0c2610d1ff3ead76233ee1b41ed862171b
Charles Moulliard <cmoulliard@apache.org>
2016-12-02 12:30:06 +0100

    Remove dockerfile as we will use s2i

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-oc delete imagestreams/front-generator
-oc delete bc/front-generator
-oc delete dc/front-generator
-oc delete service/front-generator
-oc delete route/front-generator
+# Docker Build
+# oc delete imagestreams/front-generator
+# oc delete bc/front-generator
+# oc delete dc/front-generator
+# oc delete service/front-generator
+# oc delete route/front-generator
+#
+# oc new-app https://github.com/obsidian-toaster/generator-frontend.git#npm-install --name=front-generator --strategy=docker
+#
+# oc env bc/front-generator FORGE_URL="http://generator-backend-default.192.168.64.71.xip.io/forge"
 
-oc new-app https://github.com/obsidian-toaster/generator-frontend.git --env=FORGE_URL="http://generator-backend-default.192.168.64.71.xip.io/forge" --name=front-generator --strategy=docker
+# oc create -f https://raw.githubusercontent.com/obsidian-toaster/generator-frontend/npm-install/imagestream.yaml
+# oc import-image nodejs-4
+
+oc delete imagestreams/front-generator-s2i
+oc delete bc/front-generator-s2i
+oc delete dc/front-generator-s2i
+oc delete service/front-generator-s2i
+oc delete route/front-generator-s2i
+
+oc new-app nodejs-4~https://github.com/obsidian-toaster/generator-frontend.git#npm-install --name=front-generator-s2i --strategy=source
+
+oc env bc/front-generator-s2i FORGE_URL="http://generator-backend-default.192.168.64.72.xip.io/forge"
